6d0b452bb63b70f8542a349d9e03cc8f5a6715ec
Ezra Black <dev.ezrablack@gmail.com>
2020-05-26 10:13:26 -0500

    Added Base Project

---
+++ Water My Plants/Water My Plants/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Water My Plants
+//
+//  Created by Ezra Black on 5/26/20.
+//  Copyright © 2020 Casanova Studios. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
